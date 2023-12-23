193c773d7dc846f110890bbf3a46c14cb18aa63b
Imran02120 <153354337+Imran02120@users.noreply.github.com>
2023-12-23 16:32:03 +0600

    asd
    asd

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy  import SQLAlchemy
 from datetime import datetime
 
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-
 db = SQLAlchemy(app)
 
 with app.app_context():
     db.create_all()
 
 
+
 class Article(db.Model):
-    __tablename__ = 'article'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow())
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-@app.route('/home')
-@app.route('/')
+@app.route("/")
 def index():
-    return  render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('get_posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def get_posts_detail(id):
+    article=Article.query.get(id)
 
 
 @app.route('/about')
@@ -32,76 +41,35 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/posts')
-def posts():
-    articles = Article.query.order_by(Article.date.desc()).all()
-    return render_template('posts.html', articles=articles)
-
-
-
-
-
-@app.route("/create-article", methods=["POST", "GET"])
+@app.route('/create-article', methods=["POST", "GET"])
 def create_article():
-    if request.method =="POST":
-        title = request.form["title"]
-        intro = request.form["intro"]
-        text = request.form["text"]
-
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
         # with app.app_context():
-        #     db.create_al
-
-
-
-
-
-        print(title)
-        print(intro)
-        print(text)
+        #     db.create_all()
         article = Article(title=title, intro=intro, text=text)
-
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect("/posts")
-        except Exception as e:
-            print(e)
-            return "При добавении статьи произшла неизвестная ошибка"
-
+            return redirect('/')
+        except:
+            return 'При добавлении статьи произошла ошибка'
 
 
     else:
-        return render_template("create_article.html")
-
-@app.route('/posts/<int:id>')
-def posts_detail(id):
-    article = Article.query.get(id)
-    print(article)
-    return render_template('post_detail.html', article = article)
-
-
-
-@app.route('/posts/delete/<int:id>')
-def posts_delete(id):
-    article = Article.query.get(id)
-
-
-    try:
-        db.session.delete(article)
-        db.session.commit()
-        return redirect("/posts")
-    except Exception as e:
-        print(e)
-        return "При удалении призошла ошибка"
-
-
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return f'Имя:  {name}, ID:{id}'
+        return render_template('/create-article.html')
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
 
